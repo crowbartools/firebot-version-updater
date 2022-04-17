cf486455aead5dce6660b1aef3c7fd35de5dd5f9
SReject <SReject@users.noreply.github.com>
2022-04-17 07:27:23 -0400

    use current date

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const path = require('path');
 const { readFileSync, writeFileSync } = require('fs');
+const { EOL } = require('os');
 
 const packagePath = path.resolve('./package.json');
 const package = JSON.parse(readFileSync(packagePath, 'utf-8'));
 
-package.version += `-nightly-${process.env['INPUT_SHA']}`;
+const curDate = new Date();
+
+const year = curDate.getFullYear().toString().slice(-2);
+const month = ("0" + (curDate.getMonth() + 1)).slice(-2);
+const day = ('0' + curDate.getDate()).slice(-2);
+
+const formatted = `${year}.${month}.${day}`;
+
+package.version += `-nightly-${formatted}`;
 
 writeFileSync(packagePath, JSON.stringify(package), 'utf-8');
+
+process.stdout.write(`${EOL}::set-output name=version::${package.version}${EOL}`);
 
 console.log('Version set to:', package.version);
